1a5856f9997e0226d2d7a6099613b47bbbb4a18a
ayman farres <aymanefarres@gmail.com>
2024-10-12 12:08:29 +0100

    ft

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,21 @@
-
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-   <div id='hero-section' className='text-white   z-10  sm:pt-[15rem] pt-[10rem] mx-[2rem] flex flex-col sm:gap-10 gap-2'>
-   <div>
-   <h2 className='sm:text-[1.5rem] font-name font-bold text-[1.1rem] '>
-    HI, AM AYMANE FARRES
-    </h2>
-    <h1 className='sm:text-[4rem] font-bold text-[2rem] font-arvo '>
-    Front-End Developer.
-    </h1>
-   </div>
-    <p className='sm:text-[2rem] font-par text-[1.3rem] sm:leading-[3rem] leading-[2rem] '>
-    I'm a front-end developer focused on creating responsive, engaging websites. Check out my projects to see my work in action!
-    </p>
-   </div> 
-  )
-  
-}
+    <div id='hero-section' className='text-white z-10 sm:pt-[15rem] pt-[10rem] mx-[2rem] flex flex-col sm:gap-10 gap-2'>
+      <div>
+        <h2 className='sm:text-[1.5rem] font-name font-bold text-[1.1rem]'>
+          HI, AM AYMANE FARRES
+        </h2>
+        <h1 className='sm:text-[4rem] font-bold text-[2rem] font-arvo'>
+          Front-End Developer.
+        </h1>
+      </div>
+      <p className='sm:text-[2rem] font-par text-[1.3rem] sm:leading-[3rem] leading-[2rem]'>
+        I&apos;m a front-end developer focused on creating responsive, engaging websites. Check out my projects to see my work in action!
+      </p>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
